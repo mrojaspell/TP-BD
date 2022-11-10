0ec67bb242887208add1a99862d73d889174371f
mrojaspelliccia <mrojaspelliccia@itba.edu.ar>
2022-11-10 12:59:01 -0300

    the mountain

--- functions.sql
+++ functions.sql
@@ -7,9 +7,9 @@ CREATE TABLE clientes_banco
 (
     Codigo INT NOT NULL,
     Dni INT NOT NULL,
-    Telefono INT NOT NULL,
-    Nombre char(20) NOT NULL,
-    Direccion CHAR(25) NOT NULL,
+    Telefono VARCHAR,
+    Nombre VARCHAR NOT NULL,
+    Direccion VARCHAR,
     PRIMARY KEY (Codigo)
 );
 
@@ -29,23 +29,30 @@ CREATE TABLE pagos_cuotas
     Codigo_Prestamo INT NOT NULL,
     Importe INT NOT NULL,
     Fecha DATE NOT NULL,
-    PRIMARY KEY (Nro_Cuota),
     FOREIGN KEY (Codigo_Prestamo) REFERENCES prestamos_banco
 );
 
 CREATE TABLE backup
 (
     DNI INT NOT NULL,
-    NOMBRE CHAR(20) NOT NULL,
+    NOMBRE VARCHAR NOT NULL,
     TELEFONO INT NOT NULL,
     CANT_PRESTAMOS INT NOT NULL,
     MONTO_PRESTAMOS INT NOT NULL,
-    MONTO_PAGO_CUENTAS INT NOT NULL,
+    MONTO_PAGO_CUENTAS INT,
     IND_PAGOS_PENDIENTES BOOLEAN NOT NULL,
     PRIMARY KEY (DNI)
 );
+/*
+
+para correr estos es necesesario llevar los archivos .csv a pampero
+
+
+ssh nombre@pampero.itba.edu.ar
+psql -h bd1.it.itba.edu.ar -U nombre PROOF
+
 
 \COPY clientes_banco FROM './clientes_banco.csv' DELIMITER ',' CSV HEADER;
 \COPY prestamos_banco FROM './prestamos_banco.csv' DELIMITER ',' CSV HEADER;
 \COPY pagos_cuotas FROM './pagos_cuotas.csv' DELIMITER ',' CSV HEADER;
-
+*/
